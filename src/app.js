fd4b4dc5019c70a4a2f867d235827072c17aae3e
Bhavani Chandra Vajapeyayajula <bhavanichandra9@gmail.com>
2024-10-07 10:37:44 +0530

    Cli (#1)
    * Added CLI capability
    
    * Added github action

--- src/app.js
+++ src/app.js
@@ -2,26 +2,45 @@ import express from 'express';
 import cors from 'cors';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import logger from './logger.js';
+import { Command } from 'commander';
+
+const program = new Command();
+
+// Set up command-line arguments
+program
+  .version('1.0.0')
+  .option('--api-port <number>', 'API port to listen on', '8083') // Default to 8083
+  .option('--target-url <url>', 'Target URL for the proxy')
+  .parse(process.argv);
+
+const options = program.opts();
 
 const app = express();
-const port = process.env.API_PORT || 8083
 
+// Read the options
+const API_PORT = options.apiPort;
+const TARGET_URL = options.targetUrl;
+
+if (!TARGET_URL) {
+  logger.error('Error: Target URL must be provided using --target-url');
+  process.exit(1);
+}
+
+// Set up CORS
 app.use(cors());
 
+// Set up proxy middleware
 app.use('/api', createProxyMiddleware({
-    target: process.env.API_TARGET_URL,
+    target: TARGET_URL,
     changeOrigin: true,
     logger: logger,
     secure: false,
     pathRewrite: {
         '^/api': ''
     }
-}))
+}));
 
-
-
-app.listen(port, () => {
-    logger.info("Listening to port: " + port)
+// Start the server
+app.listen(API_PORT, () => {
+    logger.info("Listening to port: " + API_PORT);
 });
-
-
